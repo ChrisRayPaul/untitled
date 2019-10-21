48663e9c1d374d8f9a76dc87927e777e2adf325d
Ray <991314542@qq.com>
2019-10-21 22:29:52 +0800

    Initial commit

---
+++ mal.c
@@ -0,0 +1,9 @@
+//
+// Created by ray on 2019/10/21.
+//
+
+#include "mal.h"
+
+void hello() {
+
+}
---
+++ mal.h
@@ -0,0 +1,8 @@
+//
+// Created by ray on 2019/10/21.
+//
+
+#ifndef UNTITLED_MAL_H
+#define UNTITLED_MAL_H
+void hello();
+#endif //UNTITLED_MAL_H
